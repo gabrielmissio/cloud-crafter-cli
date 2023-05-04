9155820a25108a46ed18b408856f00466e9f94f5
gabriel.missio <gabriel.missio@compasso.com.br>
2023-05-04 19:30:47 -0300

    feat: Add "catch" statement to "utils/openai" template

--- templates/utils/openai/src/index.js
+++ templates/utils/openai/src/index.js
@@ -3,8 +3,8 @@ require('dotenv').config()
 const openai = require('./openai')
 
 async function demo () {
-  // const { data: models } = await openai.listModels()
-  // console.log('Models', models)
+  const { data: models } = await openai.listModels()
+  console.log('Models', models)
 
   const { data: completion } = await openai.createCompletion({
     model: 'gpt-3.5-turbo',
@@ -16,4 +16,11 @@ async function demo () {
   console.log('Completion', completion)
 }
 
-demo()
+demo().catch((error) => {
+  if (error.response) {
+    console.log(error.response.status)
+    console.log(error.response.data)
+  } else {
+    console.log(error.message)
+  }
+})
