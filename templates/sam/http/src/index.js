dbffbb2fe622e685f4526c5ee27755ad8fcc38f7
gabrielmissio <gabrielmissio.dev@gmail.com>
2025-03-15 12:36:25 -0300

    refactor(sam-http): lint fix

--- templates/sam/http/src/index.js
+++ templates/sam/http/src/index.js
@@ -1,27 +1,26 @@
-let doesLambdaWarmup = false;
+let doesLambdaWarmup = false
 
 exports.handler = async (event, context) => {
-    const warmup = doesLambdaWarmup
-    
-    if (!doesLambdaWarmup) {
-        doesLambdaWarmup = true
-    }
+  const warmup = doesLambdaWarmup
 
-    return {
-        statusCode: 200,
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-        success: true,
-        warmup,
-          env: {
-            stage: process.env.STAGE,
-            appName: process.env.APP_NAME
-          },
-          event,
-          context
-        })
-    }
+  if (!doesLambdaWarmup) {
+    doesLambdaWarmup = true
+  }
+
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      success: true,
+      warmup,
+      env: {
+        stage: process.env.STAGE,
+        appName: process.env.APP_NAME
+      },
+      event,
+      context
+    })
+  }
 }
-  
