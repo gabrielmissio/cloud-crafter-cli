3c8616031b8d6f59c2f95ec33bbc9da87f137bdc
gabrielmissio <gabmissil@gmail.com>
2025-03-28 21:27:15 -0300

    feat: add "sam-http-express-esbuild-minified" template

---
+++ templates/sam/http-express-esbuild-minified/src/app.js
@@ -0,0 +1,31 @@
+require('source-map-support').install()
+
+const express = require('express')
+const router = express.Router()
+const app = express()
+
+const stage = process.env.STAGE || 'dev'
+app.disable('x-powered-by')
+app.use(express.json())
+
+router.get('/health-check', (req, res) => {
+  res.send('OK')
+})
+
+router.get('/throw-error', (req, res) => {
+  try {
+    throw new Error('This is an error')
+  } catch (error) {
+    // console.error(error)
+    console.log('-------')
+    // console.error(error.stack)
+  }
+  res.status(500).send('Error')
+})
+
+app.use(`/${stage}`, router)
+app.use('*', (req, res) => {
+  res.status(404).send(`Route '${req.originalUrl}' not found`)
+})
+
+module.exports = app
